a4a077e45d011c8af9a445c502a45715662cc640
Ulrike Felgenhauer <115288914+ufelgen@users.noreply.github.com>
2022-11-15 14:00:27 +0100

    Feature/recap day3 (#1)
    * Fetch Function
    
    * test for pull request
    
    * write createCard function
    
    * du kannst ganix
    
    * fix fetch and render functions (solution: .innerHTML is followed by = not ()
    
    * forgot to save
    
    Co-authored-by: Kirsten Schuhmann <kirsten.schuhmann@hotmail.de>

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import createCharacterCard from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,35 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+const url = "https://rickandmortyapi.com/api/character";
+
+export async function fetchCharacters() {
+  try {
+    const response = await fetch(url);
+    if (!response) {
+      console.error("Error");
+    } else {
+      const data = await response.json();
+      console.log(data);
+      console.log(data.results);
+      console.log(data.results[0].image);
+      const characters = data.results;
+      characters.forEach((character) => {
+        const card = createCharacterCard(character);
+        renderCard(card);
+      });
+    }
+  } catch (error) {
+    console.error("Du kannst ganix!!!");
+  }
+}
+
+fetchCharacters();
+
+// const characters = fetchCharacters();
+// console.log(characters);
+
+function renderCard(element) {
+  cardContainer.append(element);
+}
